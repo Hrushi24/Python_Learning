cd92624f468b25fb8c8eaf8935c193d058615248
Hrushi24 <hrushikeshjmahadik247@gmail.com>
2025-03-28 15:13:39 +0530

    Silent Auction project complete- concept used: Dictionary , control flow, exception handling(basic)

--- day 9/Silent_auction.py
+++ day 9/Silent_auction.py
@@ -1,3 +1,38 @@
 
+print("---------------------- Welcome to Silent Auction.-------------------")
 
-print("Hello")
+holder_Names = {}
+
+higestBid = 0
+higestBidder = ""
+
+auctionRunning = True
+bid = 0
+
+while(auctionRunning):
+    name = input("What is your full name : ")
+
+    try:
+        bid = int(input("Enter your bid : "))
+    except Exception:
+        print("You didn't entered amount correctly so you lost already.")
+        bid = 0
+
+    holder_Names[name] = bid
+
+    choice = input("Are there any more bidder? (y/n) : ")
+
+    if(choice == 'n'):
+        auctionRunning = False
+    else:
+        print("\n" * 20)
+        auctionRunning = True
+
+
+for bidder in holder_Names:
+    if(higestBid <= holder_Names[bidder]):
+        higestBid = holder_Names[bidder]
+        higestBidder = bidder
+
+print("\n" * 3)
+print(f"Higest bidder is {higestBidder} with bid of {higestBid}Rs.")
