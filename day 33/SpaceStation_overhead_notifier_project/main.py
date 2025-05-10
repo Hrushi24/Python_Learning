ca745d535f2d09973aa4a038cdc475857bdb1fb0
Hrushi24 <hrushikeshjmahadik247@gmail.com>
2025-05-10 12:51:47 +0530

    SpaceStation notifier complete. Did not include email feature.

--- day 33/SpaceStation_overhead_notifier_project/main.py
+++ day 33/SpaceStation_overhead_notifier_project/main.py
@@ -12,6 +12,10 @@ iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
 #Your position is within +5 or -5 degrees of the ISS position.
+if(iss_latitude < MY_LAT + 5.0 or iss_latitude > MY_LAT - 5.0):
+    near_Lati = True
+if(iss_longitude < MY_LONG + 5.0 or iss_latitude > MY_LONG - 5.0):
+    near_Longi = True
 
 
 parameters = {
@@ -26,12 +30,15 @@ data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+time_now = datetime.now().hour
 
 #If the ISS is close to my current position
 # and it is currently dark
 # Then send me an email to tell me to look up.
 # BONUS: run the code every 60 seconds.
 
-
+if(sunset < time_now < sunrise) and (iss_latitude and iss_longitude):
+    print("go see iss above your head.")
+else:
+    print("ISS not near you buddy!!")
 
